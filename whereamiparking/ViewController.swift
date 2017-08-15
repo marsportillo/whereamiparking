482132ed537600aa9224e7310c65cfc98b4c0d57
Marco Sportillo <msportillo@gmail.com>
2017-08-15 20:31:39 +0200

    first commit

---
+++ whereamiparking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  whereamiparking
+//
+//  Created by marco sportillo on 15/08/2017.
+//  Copyright © 2017 marcosportillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
